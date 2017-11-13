3f771ed294fc6baf95dc7c9af96f3b891c39ff1e
Ryunosuke1648 <mest16akgx84ru@docomo.ne.jp>
2017-11-14 01:30:58 +0900

    コマンドを作成して、特徴のあるデータを処理できるようになりました

--- brickset_app/item/admin.py
+++ brickset_app/item/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Item, WishListモデルをインポート
+from .models import Item, WishList
+
+# 管理サイトへのモデルの登録
+admin.site.register(Item)
+admin.site.register(WishList)
